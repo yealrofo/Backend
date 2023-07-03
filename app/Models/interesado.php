9f31bbc4b8de3fb5f9b42188097b6b2f58228995
YARF <yealrofo@gmail.com>
2023-07-02 22:27:12 -0500

    primer guardado db migration

---
+++ app/Models/interesado.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class interesado extends Model
+{
+    use HasFactory;
+    $protected $table = "interesados";
+    $protected $table = "id";
+    $protected $fillable = [ "name","empresa","cargo","vacante","correo","telefono"];
+    
+}
